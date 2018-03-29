38b548b0775bfc41dc7ef0297dac580e9cfc0440
Melena Suliteanu <melenasuliteanu@gmail.com>
2018-03-28 20:01:41 -0600

    Add endpoint for getting a specific organism by id

--- server.js
+++ server.js
@@ -8,15 +8,41 @@ const database = require('knex')(configuration);
 
 app.set('port', process.env.PORT || '3000')
 
+function* offsetGenerator() {
+  yield 0;
+
+  let offset = 0;
+  while(offset < 19000) {
+    yield offset += 20;
+  }
+}
+const gen = offsetGenerator();
+
 app.get('/api/v1/organisms', (request, response) => {
-  database('organisms').select().limit(20).offset(0)
+  const offset = gen.next().value;
+  database('organisms').select().limit(20).offset(offset)
     .then(organisms => {
-      response.status(200).json(organisms)
+      return response.status(200).json(organisms);
     })
     .catch(err => {
-      response.status(500).json({error})
+      return response.status(500).json({error});
     })
-  // knex.select('*').from('users').limit(10).offset(30)
+})
+
+app.get('/api/v1/organisms/:id', (request, response) => {
+  database('organisms').where('id', request.params.id).select()
+  .then(organism => {
+    if(organism) {
+      return response.status(200).json(organism);
+    } else {
+      return response.status(404).json({
+        error: `Could not find organism with id: ${request.params.id}`
+      })
+    }
+  })
+  .catch(err => {
+    response.status(500).json({err})
+  })
 })
 
 app.listen(app.get('port'), () => {
